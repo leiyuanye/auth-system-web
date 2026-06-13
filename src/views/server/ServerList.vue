a59eb5eb78e0f8e200913139665bd1d2320ad8e4
leiyuanye <leiyuanye@users.noreply.github.com>
2026-06-13 08:32:48 +0000

    chore: 到期时间显示格式改为YYYY/MM/DD

--- src/views/server/ServerList.vue
+++ src/views/server/ServerList.vue
@@ -246,7 +246,7 @@ function formatDate(t) {
     const y = d.getFullYear()
     const m = String(d.getMonth() + 1).padStart(2, '0')
     const day = String(d.getDate()).padStart(2, '0')
-    return `${y}年${m}月${day}日`
+    return `${y}/${m}/${day}`
   } catch (e) { return String(t) }
 }
 
